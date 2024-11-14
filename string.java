84fa0f136c2f6ed7e4e48908f78bd1f7174684b2
kavinmathi123 <kavinmathianandhakumar@gmail.com>
2024-11-14 11:56:22 +0530

    String and logical operator

---
+++ string.java
@@ -0,0 +1,105 @@
+
+// in this case f1 and f2 not check the content that check the referenc address f1 and f2 address is different
+// in this case a1 create a value is orange but a2 not create value orange only refer al address so a1 and a2 address is same.
+    
+// class string{
+//     public static void main(String args[]){
+//         String f1=new String("apple");  // object
+//         String f2=new String("apple"); 
+
+//         String a1="orange"; 
+//         String a2="orange";
+
+//         System.out.println(f1==f2);  
+//         System.out.println(a1==a2); 
+//     }
+// }
+
+// use equals function
+
+// class string{
+//     public static void main(String args[]){
+//         String f1=new String("apple");  // object
+//         String f2=new String("apple"); 
+
+//         System.out.println(f1.equals(f2));  
+//     }
+// }
+
+// q1
+
+
+// import java.util.Scanner;
+// class string{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         String RCB=sc.nextLine();
+//         if(RCB.equals("Win"))
+//         {
+//             System.out.println("Ee sala cup namdhey");
+//         }
+//         else
+//         {
+//             System.out.println("cup illa");
+//         }
+//     }
+// }
+
+//q2
+// import java.util.Scanner;
+// class string{
+//     public static void main(String args[]){
+//        Scanner sc=new Scanner(System.in);
+//        int mark=sc.nextInt();
+//        if(mark>=35){
+//         System.out.println("Pass");
+//        } 
+//        else
+//        {
+//         System.out.println("Fail");
+//        }
+//     }
+// }
+
+//q3
+
+// import java.util.Scanner;
+// class string{
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         int income=sc.nextInt();
+//         if(income>7000)
+//         {
+//             System.out.println("Scholership is available");
+//         }
+//         else
+//         {
+//             System.out.println("Scholarship is not available");
+//         }
+//     }
+// }
+
+
+//q4
+
+import java.util.Scanner;
+class string{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even number");
+        }
+        else
+        {
+            System.out.println("Odd number");
+        }
+    }
+}
+
+
+
+
+
+
